f28b4180dc7965840b06a5d20003b8c22be645b6
Shahin Khosh BIn Ghomash <67626248+khoshsha@users.noreply.github.com>
2021-01-14 12:55:44 +0100

    bug fix

--- interpolate.py
+++ interpolate.py
@@ -195,7 +195,9 @@ class RasterInterpolator(object):
                 values.append(ident.results()[1])
                 x += xres
             y -= yres
-        total=0
+        for v in values:
+            if v is None:
+                return self.noDataValue
         value=max(values)
         return value
 
@@ -223,6 +225,8 @@ class RasterInterpolator(object):
                 values.append(ident.results()[1])
                 x += xres
             y -= yres
-        total=0
+        for v in values:
+            if v is None:
+                return self.noDataValue
         value=min(values)
         return value
